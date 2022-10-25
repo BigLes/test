804c944c72c83cafa72f902598999a95adeeb5cd
Oleksandr Lisovyk <lisovykoleksandr@gmail.com>
2022-10-25 21:05:22 +0300

    Fix errors on user deletion

--- src/pages/groupProfile.tsx
+++ src/pages/groupProfile.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useMemo } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { useAppDispatch, useAppSelector } from '../hooks/store';
 import { deleteGroup } from '../store/groupsSlice';
@@ -12,27 +12,31 @@ export const GroupProfile = () => {
   const users = useAppSelector(state => state.users.items);
 
   const idNumber = parseInt(params.id as string, 10);
-  const selectedGroup = items.filter(item => item.id === idNumber)[0];
+  const selectedGroup = useMemo(() => items.filter(item => item.id === idNumber)[0], [idNumber, items]);
+
+  useEffect(() => {
+    if (!selectedGroup) {
+      navigate('/');
+    }
+  }, [navigate, selectedGroup])
 
   const groupUser = users.filter(item => item.groups.includes(idNumber))
 
   return <div>
-    <h3 className="text-center">{selectedGroup.name}</h3>
+    <h3 className="text-center">{selectedGroup?.name}</h3>
     <ul className="overflow-y-auto">{
       groupUser
         .map(item => (
           <li
             onClick={() => dispatch(toggleGroupForUser({ userId: item.id, groupId: idNumber }))}
             className="h-16 w-44 text-center p-2 border border-slate-300 rounded mb-2 cursor-pointer"
-            key={item.id}>{item.name}</li>
+            key={item.id}>{item?.name}</li>
         ))
     }</ul>
     {!groupUser.length && (
       <div className="flex justify-center w-full mt-4">
         <button
-          onClick={() => {
-            dispatch(deleteGroup(idNumber)).then(() => navigate('/'))
-          }}
+          onClick={() => dispatch(deleteGroup(idNumber))}
           className="text-center p-2 border border-red-500 rounded mb-2">DELETE GROUP</button>
       </div>
     )}
--- src/pages/userProfile.tsx
+++ src/pages/userProfile.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useMemo } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { useAppDispatch, useAppSelector } from '../hooks/store';
 import { deleteUser, toggleGroupForUser } from '../store/usersSlice';
@@ -13,15 +13,21 @@ export const UserProfile = () => {
   const groups = useAppSelector(state => state.groups.items);
 
   const idNumber = parseInt(params.id as string, 10);
-  const selectedUser = items.filter(item => item.id === idNumber)[0];
+  const selectedUser = useMemo(() => items.filter(item => item.id === idNumber)[0], [idNumber, items]);
 
-  const userGroups = groups.filter(item => selectedUser.groups.includes(item.id));
-  const nonUserGroups = groups.filter(item => !selectedUser.groups.includes(item.id));
+  useEffect(() => {
+    if (!selectedUser) {
+      navigate('/');
+    }
+  }, [navigate, selectedUser])
+
+  const userGroups = groups.filter(item => selectedUser?.groups.includes(item.id));
+  const nonUserGroups = groups.filter(item => !selectedUser?.groups.includes(item.id));
 
   const options = nonUserGroups.map(item => ({ value: item.id, label: item.name }))
 
   return <div>
-    <h3 className="text-center">{selectedUser.name}</h3>
+    <h3 className="text-center">{selectedUser?.name}</h3>
     <div className="mb-2">
       <h4 className="inline mr-2">Assign group:</h4>
       <Select
@@ -37,7 +43,7 @@ export const UserProfile = () => {
           <li
             onClick={() => dispatch(toggleGroupForUser({ userId: idNumber, groupId: item.id }))}
             className="h-16 w-44 text-center p-2 border border-slate-300 rounded mb-2 cursor-pointer"
-            key={item.id}>{item.name}</li>
+            key={item.id}>{item?.name}</li>
         ))
     }</ul>
     <div className="flex justify-center w-full mt-4">
